38cc6ace8ce7d0f010e7e684ded848cc871ba43c
ambouh <andresmbouh@gmail.com>
2019-06-21 15:28:44 -0400

    Updates

---
+++ precache-manifest.bf2098ced64464b55792f857de397df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "093dd1a89707b25fd176c87b9c2ea203",
+    "url": "/betelle-company/static/media/mission.093dd1a8.png"
+  },
+  {
+    "revision": "204b68cb30f8cf3a2c6be1cf1f795178",
+    "url": "/betelle-company/static/media/jumboBG.204b68cb.png"
+  },
+  {
+    "revision": "f008b9f8dfc84e777e6333b86d515b90",
+    "url": "/betelle-company/static/media/Service.f008b9f8.bin"
+  },
+  {
+    "revision": "4136e793581c391d28a3",
+    "url": "/betelle-company/static/js/runtime~main.4136e793.js"
+  },
+  {
+    "revision": "5dc4eb26e3078059e806",
+    "url": "/betelle-company/static/js/main.5dc4eb26.chunk.js"
+  },
+  {
+    "revision": "7c88bf86c45c15028aac",
+    "url": "/betelle-company/static/js/1.7c88bf86.chunk.js"
+  },
+  {
+    "revision": "5dc4eb26e3078059e806",
+    "url": "/betelle-company/static/css/main.e4bdec66.chunk.css"
+  },
+  {
+    "revision": "af1ccb31a671c738f6980fb4127fa861",
+    "url": "/betelle-company/index.html"
+  }
+];
