e2d3840a3bd116ed44000069f3e901d18e447634
Andres Mbouh <andresmbouh@gmail.com>
2019-11-13 15:56:28 -0500

    about: the entire page to be responsive(2)

--- src/components/About/valueDefinition.js
+++ src/components/About/valueDefinition.js
@@ -6,40 +6,34 @@ import underline from "../../images/values-border.png";
 
 const ValueDefinition = () => {
     return (
-        <div className={"aboutValueDfn columns-span-2"}>
-            <div className={"equal"}>
-                <div className={"img-collage"}>
-                    {/*
-                    <img className={"pic2"} src={pic2}/>
-                    <img className={"pic3"} src={pic3}/>
-                    <img className={"pic1"} src={pic1}/>
-                    */}
-                    <div className={"div1"}>
-                        <img className={"pic1"} src={pic1}/>
-                    </div>
-                    <div className={"div2"}>
-                        <img className={"pic2"} src={pic2}/>
-                    </div>
-                    <div className={"div3"}>
-                        <img className={"pic3"} src={pic3}/>
+        <div className={"aboutValueDfn row"}>
+              <div className={"image-collage"}>
+                    <div className={"row"}>
+                        <div className={"col1"}>
+                            <div className={"div1"}>
+                                <img className={"pic1"} src={pic1}/>
+                            </div>
+                            <div className={"div3"}>
+                                <img className={"pic3"} src={pic3}/>
+                            </div>
+                        </div>
+                        <div className={"col2"}>
+                            <div className={"div2"}>
+                                <img className={"pic2"} src={pic2}/>
+                            </div>
+
+                            <h1 className={"ourValues_lg"}>Our Values
+                                <img src={underline} alt={"Our Values"}/>
+                            </h1>
+                        </div>
                     </div>
                 </div>
-                <h1 className={"ourValues_lg"}>Our Values
-                    <img src={underline} alt={"Our Values"}/>
-                </h1>
-
-            </div>
-
-              <div className={"equal"}>
-                <h1 className={"ourValues"}>Our Values
-                <img src={underline} alt={"Our Values"}/>
-                </h1>
+              <div className={"value-section"}>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                 </p>
             </div>
-           {/* */}
         </div>
     );
 };
