2f3805fdaddcba872b6dfcfcca70587d9f96a7c6
Andres Mbouh <andresmbouh@gmail.com>
2020-01-18 11:27:53 -0500

    modifying media to be lg to sm(2)

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/betelle-company/precache-manifest.c2a122114f784ad2f14639ff631253bf.js"
+  "/betelle-company/precache-manifest.57ebaf528f5ba92552fc029f506ca8c4.js"
 );
 
 workbox.clientsClaim();
